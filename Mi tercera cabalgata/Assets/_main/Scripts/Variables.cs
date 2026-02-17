a40babd538b4ed60d859711d5fbbf8eb7cb340f7
cusnico <nicocoloradozab@gmail.com>
2026-02-17 12:37:56 -0500

    Variables 2
    -Concatenar texto.
    -IU.

---
+++ Mi tercera cabalgata/Assets/_main/Scripts/Variables.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Variables : MonoBehaviour
+{
+    //Declarar variables
+
+    public string myName = "Nicol";
+    public int edad = 18;
+    public bool isBool;
+
+    [SerializeField] private TMP_InputField _inputfield;
+    [SerializeField] private TMP_Text _saludo;
+    [SerializeField] private Renderer _cubo;
+    [SerializeField] private BoxCollider _boxCollider;
+
+   
+
+    // Start is called before the first frame update 
+    void Start()
+    {
+        _cubo.material.color = Color.blue;
+        myName = _inputfield.text;
+        _saludo.text = "Hola " + myName;
+        _boxCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myName = _inputfield.text;
+        _saludo.text = "Hola " + myName;
+    }
+}
